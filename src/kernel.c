b045a782703c775c4e546c50853ebffdc1987981
RealDoomGaming <8marcus.h8@protonmail.com>
2026-08-06 13:02:49 +0200

    made the kernel print some stuff like a success message, and the screen gets cleared

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,40 @@
+#include <stdint.h>
+
+// we need to use the uint16_t* type here since for vga every character is stored as 16bit and not like normally as 8bit
+#define VGA ((volatile uint16_t*) 0xB8000)
+// width and height of the screen in characters
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+// color
+#define WHITE_ON_BLACK 0x0F
+
+void print(const char msg[], const int msg_length, const int color) {
+    for (int i = 0; i < msg_length; i++) {
+        // here we combine the char and the color of the char into a 16 bit entry which we will use to clear the screen later
+        uint16_t combined = msg[i] | (color << 8);
+        VGA[i] = combined;
+    } 
+}
+
+void clear_screen() {
+    // here we combine the char and the color of the char into a 16 bit entry which we will use to clear the screen later
+    uint16_t blank = ' ' | (WHITE_ON_BLACK << 8);
+
+    for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
+        VGA[i] = blank;
+    }
+}
+
 void kernel_main() {
-    // our vga output address so we can print something later
-    volatile char* vga = (volatile char*)0xB8000;
+    // first thing we do is clear the screen of any leftover text
+    clear_screen();
+
+    // this is our success message string
+    const char success_msg[] = "Successfully loaded the kernel. 13 10 0";
+    const int succ_msg_length = 31;
+    const int color = WHITE_ON_BLACK;
+
+    print(success_msg, succ_msg_length, color);
 
     // while loop so the cpu doesnt run off into memory junk
     while (1) {
